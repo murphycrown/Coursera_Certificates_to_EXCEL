cba6373df5552816fbb87b08d7a7d6d4587271d4
murphycrown <mirzezademehemmed2020@gmail.com>
2026-03-27 03:48:55 +0400

    v 1.0

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from soup import verificationlinkSaver
 from createExcel import createExcel
 import json
 import math
-num=int(input('How many certificates you have:'))
+num=int(input('How many certificates you have: '))
 count=math.ceil(num/10)
-cookie=input("Enter the cookie:")
-print("\nProcess starting....\n")
+cookie=input("Enter your Coursera Cookie:")
+print("Process starting....\n")
 parsed_cookie=parser(cookie)
 
 for i in range(0,(count*10)+1,10):
@@ -18,14 +18,14 @@ for i in range(0,(count*10)+1,10):
     print(response)
     linkextract(json.loads(response))
 #Verification link extract
-print("Certificates' names and target_urls are saved ")
+print("Certificates' names and target_urls saved ")
 print("Verification links' saving is starting.....")
 with open('target_urls.txt','r') as file:
    for target in file.read().split():
     response = makeRequestLink(target)
     context=json.loads(response)
     verificationlinkSaver(context)
-print("Verification links are saved")
+print("Verification links saved")
 print("Generating Excel file ....")
 createExcel()
 print("Excel file created succesfully!!!")
